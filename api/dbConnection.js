50769c35f5f187ecaf4127953e32296caa26588f
anshvishnoi04 <114725557+anshvishnoi04@users.noreply.github.com>
2024-09-02 09:54:27 +0530

    Update dbConnection.js

--- api/dbConnection.js
+++ api/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const uri =
-  "mongodb+srv://omshukla:1234@cluster0.2hott.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =mongodb+srv://name:<db_password>@cluster0.hqsdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+  
 mongoose;
 
 function dbConnection() {
